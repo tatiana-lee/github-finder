a2de75ccb5aa03cd709074785d0ddc18e18eeb17
satania-lee <rockky.tl@gmail.com>
2022-06-09 16:13:50 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import Alert from './components/layout/Alert'
 import About from './pages/About'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './context/github/GithubContex'
+import { AlertProvider } from './context/alert/AlertContext'
+import User from './pages/User'
 
 function App() {
 	return (
 		<GithubProvider>
-			<BrowserRouter>
-				<div className='flex flex-col justify-between h-screen'>
-					<Navbar />
+			<AlertProvider>
+				<BrowserRouter>
+					<div className='flex flex-col justify-between h-screen'>
+						<Navbar />
 
-					<main className='container mx-auto px-3 pb-12'>
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/about' element={<About />} />
-							<Route path='/notfound' element={<NotFound />} />
-							<Route path='/*' element={<NotFound />} />
-						</Routes>
-					</main>
+						<main className='container mx-auto px-3 pb-12'>
+						<Alert />
+							<Routes>
+								<Route path='/' element={<Home />} />
+								<Route path='/about' element={<About />} />
+								<Route path='/user/:login' element={<User />} />
+								<Route path='/notfound' element={<NotFound />} />
+								<Route path='/*' element={<NotFound />} />
+							</Routes>
+						</main>
 
-					<Footer />
-				</div>
-			</BrowserRouter>
+						<Footer />
+					</div>
+				</BrowserRouter>
+			</AlertProvider>
 		</GithubProvider>
 	)
 }
